c10f6e964d6a61b36d06018457c2bbaff784e4c7
Helio Ortega <helio.ortega.neto@gmail.com>
2024-04-24 12:50:09 -0300

    Implementacao de teste unitarios com PHPUnit TestCase

--- app_carrinho_compras_b/vendor/composer/autoload_real.php
+++ app_carrinho_compras_b/vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInit208da8d7dc535898eb6d82eb330ff51c
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit208da8d7dc535898eb6d82eb330ff51c::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
